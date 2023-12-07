6ba6c4dccf5c0d39ef9a4363372a96a7400b9862
wadenya <alexwadenya001@gmail.com>
2023-12-07 10:12:56 +0000

    Python - Variable Annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,7 @@
 '''type-annotated function concat that takes a string str1 and a string str2
 '''
 
+
 def concat (str1: str, str2: str) -> str:
     '''returns a concatenated string'''
     return str1 + str2
